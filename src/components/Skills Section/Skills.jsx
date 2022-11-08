dc64f75ac75568cb4e1dd1cbffaa195e11b55492
Preetiraj3697 <satendra11kum@gmail.com>
2022-11-08 11:57:10 +0530

    add

--- src/components/Skills Section/Skills.jsx
+++ src/components/Skills Section/Skills.jsx
@@ -4,12 +4,12 @@ import { IoLogoCss3, IoLogoJavascript, IoLogoNodejs, IoLogoSass, IoLogoWordpress
 import './skills.css'
 import GitHubCalendar from 'react-github-calendar';
 import { AiFillGithub, AiFillGitlab, AiFillHtml5 } from 'react-icons/ai'
-import {GrMysql} from 'react-icons/gr'
-import {SiBootstrap, SiChakraui, SiCypress, SiExpress, SiFigma, SiMongodb, SiRedux, SiStyledcomponents, SiTailwindcss} from 'react-icons/si'
+import { GrMysql } from 'react-icons/gr'
+import { SiBootstrap, SiChakraui, SiCypress, SiExpress, SiFigma, SiMongodb, SiRedux, SiStyledcomponents, SiTailwindcss } from 'react-icons/si'
 const Skills = () => {
   return (
     <section id="skills" className='skills container section'>
-       <div className="sectionTitle">
+      <div className="sectionTitle">
         <span className="titleNumber">02</span>
         <h5 className="titleText">
           Skills
@@ -18,158 +18,158 @@ const Skills = () => {
           </div>
         </h5>
       </div>
-    {/* skills container */}
-    <div className="skillsContainer grid">
-      <div className="skillGroup">
-        <div className="groupTitle">
-          <h2 className="title">FrontEnd</h2>
-        </div>
-        <div className="generalSkills">
-        <div className="singleSkills">
-          <div className="iconBox flex">
-            <AiFillHtml5 className='icon' />
-          </div>
-          <span className="skillName">HTML5</span>
-        </div>
-        <div className="singleSkills">
-          <div className="iconBox flex">
-            <IoLogoCss3 className='icon' />
-          </div>
-          <span className="skillName">Css3</span>
-        </div>
-        <div className="singleSkills">
-          <div className="iconBox flex">
-            <SiStyledcomponents className='icon' />
-          </div>
-          <span className="skillName">Styled<br></br>Component</span>
-        </div>
-        <div className="singleSkills">
-          <div className="iconBox flex">
-            <IoLogoSass className='icon' />
-          </div>
-          <span className="skillName">Sass</span>
-        </div>
-        <div className="singleSkills">
-          <div className="iconBox flex">
-            <IoLogoJavascript className='icon' />
-          </div>
-          <span className="skillName">JavaScript</span>
-        </div>
-        <div className="singleSkills">
-          <div className="iconBox flex">
-            <TbBrandReactNative className='icon' />
-          </div>
-          <span className="skillName">React</span>
-        </div>
-        
-      </div>
-      </div>
-      <div className="skillGroup">
-        <div className="groupTitle">
-          <h2 className="title">BackEnd</h2>
-        </div>
-        <div className="generalSkills">
-        <div className="singleSkills">
-          <div className="iconBox flex">
-            <SiRedux className='icon' />
-          </div>
-          <span className="skillName">Redux</span>
-        </div>
-        <div className="singleSkills">
-          <div className="iconBox flex">
-            <IoLogoNodejs className='icon' />
-          </div>
-          <span className="skillName">NodeJs</span>
-        </div>
-        <div className="singleSkills">
-          <div className="iconBox flex">
-            <SiExpress className='icon' />
-          </div>
-          <span className="skillName">ExpressJs</span>
-        </div>
-        <div className="singleSkills">
-          <div className="iconBox flex">
-            <SiMongodb className='icon' />
-          </div>
-          <span className="skillName">MongoDB</span>
-        </div>
-        <div className="singleSkills">
-          <div className="iconBox flex">
-            <SiCypress className='icon' />
-          </div>
-          <span className="skillName">Cypress</span>
-        </div>
-        <div className="singleSkills">
-          <div className="iconBox flex">
-            <GrMysql className='icon' />
-          </div>
-          <span className="skillName">Mysql</span>
-        </div>
-      </div>
-      </div>
+      {/* skills container */}
+      <div className="skillsContainer grid">
         <div className="skillGroup">
-        <div className="groupTitle">
-          <h2 className="title">Other</h2>
-        </div>
-        <div className="generalSkills">
-        <div className="singleSkills">
-          <div className="iconBox flex">
-            <AiFillGithub className='icon' />
+          <div className="groupTitle">
+            <h2 className="title">FrontEnd</h2>
           </div>
-          <span className="skillName">GitHub</span>
-        </div>
-        <div className="singleSkills">
-          <div className="iconBox flex">
-            <AiFillGitlab className='icon' />
+          <div className="generalSkills">
+            <div className="singleSkills">
+              <div className="iconBox flex">
+                <AiFillHtml5 className='icon' />
+              </div>
+              <span className="skillName">HTML5</span>
+            </div>
+            <div className="singleSkills">
+              <div className="iconBox flex">
+                <IoLogoCss3 className='icon' />
+              </div>
+              <span className="skillName">Css3</span>
+            </div>
+            <div className="singleSkills">
+              <div className="iconBox flex">
+                <SiStyledcomponents className='icon' />
+              </div>
+              <span className="skillName">Styled<br></br>Component</span>
+            </div>
+            <div className="singleSkills">
+              <div className="iconBox flex">
+                <IoLogoSass className='icon' />
+              </div>
+              <span className="skillName">Sass</span>
+            </div>
+            <div className="singleSkills">
+              <div className="iconBox flex">
+                <IoLogoJavascript className='icon' />
+              </div>
+              <span className="skillName">JavaScript</span>
+            </div>
+            <div className="singleSkills">
+              <div className="iconBox flex">
+                <TbBrandReactNative className='icon' />
+              </div>
+              <span className="skillName">React</span>
+            </div>
+
           </div>
-          <span className="skillName">Git</span>
         </div>
-        <div className="singleSkills">
-          <div className="iconBox flex">
-            <SiFigma className='icon' />
+        <div className="skillGroup">
+          <div className="groupTitle">
+            <h2 className="title">BackEnd</h2>
           </div>
-          <span className="skillName">Figma</span>
-        </div>
-        <div className="singleSkills">
-          <div className="iconBox flex">
-            <IoLogoWordpress className='icon' />
+          <div className="generalSkills">
+            <div className="singleSkills">
+              <div className="iconBox flex">
+                <SiRedux className='icon' />
+              </div>
+              <span className="skillName">Redux</span>
+            </div>
+            <div className="singleSkills">
+              <div className="iconBox flex">
+                <IoLogoNodejs className='icon' />
+              </div>
+              <span className="skillName">NodeJs</span>
+            </div>
+            <div className="singleSkills">
+              <div className="iconBox flex">
+                <SiExpress className='icon' />
+              </div>
+              <span className="skillName">ExpressJs</span>
+            </div>
+            <div className="singleSkills">
+              <div className="iconBox flex">
+                <SiMongodb className='icon' />
+              </div>
+              <span className="skillName">MongoDB</span>
+            </div>
+            <div className="singleSkills">
+              <div className="iconBox flex">
+                <SiCypress className='icon' />
+              </div>
+              <span className="skillName">Cypress</span>
+            </div>
+            <div className="singleSkills">
+              <div className="iconBox flex">
+                <GrMysql className='icon' />
+              </div>
+              <span className="skillName">Mysql</span>
+            </div>
           </div>
-          <span className="skillName">Wordpress</span>
         </div>
-        <div className="singleSkills">
-          <div className="iconBox flex">
-            <SiChakraui className='icon' />
+        <div className="skillGroup">
+          <div className="groupTitle">
+            <h2 className="title">Other</h2>
           </div>
-          <span className="skillName">ChakraUI</span>
-        </div>
-        <div className="singleSkills">
-          <div className="iconBox flex">
-            <SiBootstrap className='icon' />
+          <div className="generalSkills">
+            <div className="singleSkills">
+              <div className="iconBox flex">
+                <AiFillGithub className='icon' />
+              </div>
+              <span className="skillName">GitHub</span>
+            </div>
+            <div className="singleSkills">
+              <div className="iconBox flex">
+                <AiFillGitlab className='icon' />
+              </div>
+              <span className="skillName">Git</span>
+            </div>
+            <div className="singleSkills">
+              <div className="iconBox flex">
+                <SiFigma className='icon' />
+              </div>
+              <span className="skillName">Figma</span>
+            </div>
+            <div className="singleSkills">
+              <div className="iconBox flex">
+                <IoLogoWordpress className='icon' />
+              </div>
+              <span className="skillName">Wordpress</span>
+            </div>
+            <div className="singleSkills">
+              <div className="iconBox flex">
+                <SiChakraui className='icon' />
+              </div>
+              <span className="skillName">ChakraUI</span>
+            </div>
+            <div className="singleSkills">
+              <div className="iconBox flex">
+                <SiBootstrap className='icon' />
+              </div>
+              <span className="skillName">Bootstrap</span>
+            </div>
           </div>
-          <span className="skillName">Bootstrap</span>
         </div>
-      </div>
-      </div>
       </div>
       <br></br>
       <div class="education">
         <h1>Preeti Raj Github Statistics</h1>
         <div class="cal-con">
-            <div class="calendar">
-            <GitHubCalendar username="Preetiraj3697" year={new Date().getFullYear()}  blockMargin={7}
-            //  blockRadius={5}
-            //  blockSize={20}
-            //  fontSize={34}
-             color="#00eaff" 
-             responsive={true}/>
-            </div>
-        </div>
-        <div class="stats">
+          <div class="calendar">
+            <GitHubCalendar username="Preetiraj3697" year={new Date().getFullYear()} blockMargin={7}
+              color="#00eaff"
+              responsive={true} />
+          </div>
+          <div><a href="https://github.com/Preetiraj3697/github-readme-activity-graph"><img alt="Preetiraj3697 Activity Graph" src="https://activity-graph.herokuapp.com/graph?username=Preetiraj3697&bg_color=050f2c&color=2ddd98&line=5BCDEC&point=FFFFFF&hide_border=false&show_icons=true&radius=10&area=true&area_color=2ddd98&border=3" /></a></div>
+          <div class="stats">
+            
             <img
-                src="https://github-readme-stats.vercel.app/api?username=Preetiraj3697&theme=algolia&hide_border=false&include_all_commits=true&count_private=true&show_icons=true" />
+              src="https://github-readme-stats.vercel.app/api?username=Preetiraj3697&theme=algolia&hide_border=false&include_all_commits=true&count_private=false&show_icons=true" />
+          </div>
         </div>
-    </div>
-  </section>
+
+      </div>
+    </section>
   )
 }
 
